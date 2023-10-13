b723b141bc50e13e3c94f985376d087dac081d03
ma3722 <ma3722@ic.ac.uk>
2023-10-13 17:52:46 +0100

    ma3722, aaa1322 | added list_less_func for ordering ready list by priority

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -597,3 +597,12 @@ allocate_tid (void)
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
+
+/* list_less_func for ordering by non-decreasing priority. */
+static bool priority_list_less_func(const struct list_elem *a,
+                                    const struct list_elem *b,
+                                    void *aux UNUSED) {
+  struct thread *thread_a = list_entry(a, struct thread, elem);
+  struct thread *thread_b = list_entry(b, struct thread, elem);
+  return thread_a -> priority > thread_b -> priority;
+}
