bb13bf1b32cc38f981d14e23d1929682a6b31ff6
Shaban Kashif <mk2520@imperial.ac.uk>
2023-11-05 18:32:21 +0000

    Added static system call function placeholders for syscall_handler() use and future implementation.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,10 +6,25 @@
 
 static void syscall_handler (struct intr_frame *);
 
+static void sys_halt(void);
+static void sys_exit(int status);
+static pid_t sys_exec(const char *file);
+static int sys_wait(pid_t pid);
+static bool sys_create(const char *file, unsigned initial_size);
+static bool sys_remove(const char *file);
+static int sys_open(const char *file);
+static int sys_filesize(int fd);
+static int sys_read(int fd, void *buffer, unsigned size);
+static int sys_write(int fd, const void *buffer, unsigned size);
+static void sys_seek(int fd, unsigned position);
+static unsigned sys_tell(int fd);
+static void sys_close(int fd);
+
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  syst();
 }
 
 static void
