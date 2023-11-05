bfab441af1004193043c489b7dbe0b4d6dd38495
Shaban Kashif <mk2520@imperial.ac.uk>
2023-11-05 19:31:04 +0000

    Implemented basic syscall_handler() that passes itself to correct function depending on syscall value.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,7 @@
 
 static void syscall_handler (struct intr_frame *);
 
+/* System call functions. */
 static void sys_halt(void);
 static void sys_exit(int status);
 static pid_t sys_exec(const char *file);
@@ -30,6 +31,53 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  /* Creates a pointer to 32 bit system call number from SP and dereferences it. */
+  uint32_t *syscall_num = (uint32_t *) f->esp;
+  uint32_t syscall_number = *syscall_num;
+
+  /* Switch statement to handle each system call depending on syscall_number value. */
+  // Values currently NULL as each call has yet to be implemented (delete comment once done).
+  switch (syscall_number) {
+    case SYS_HALT:
+      sys_halt();
+      break;
+    case SYS_EXIT:
+      sys_exit(NULL);
+      break;
+    case SYS_EXEC:
+      sys_exec(NULL);
+      break;
+    case SYS_WAIT:
+      sys_wait(NULL);
+      break;
+    case SYS_CREATE:
+      sys_create(NULL, NULL);
+      break;
+    case SYS_REMOVE:
+      sys_remove(NULL);
+      break;
+    case SYS_OPEN:
+      sys_open(NULL);
+      break;
+    case SYS_FILESIZE:
+      sys_filesize(NULL);
+      break;
+    case SYS_READ:
+      sys_read(NULL, NULL, NULL);
+      break;
+    case SYS_WRITE:
+      sys_write(NULL, NULL, NULL);
+      break;
+    case SYS_SEEK:
+      sys_seek(NULL, NULL);
+      break;
+    case SYS_TELL:
+      sys_tell(NULL);
+      break;
+    case SYS_CLOSE:
+      sys_close(NULL);
+      break;  
+    default:
+      thread_exit(); // Terminates thread if unsupported call given.
+  }
 }
