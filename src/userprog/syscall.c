aa79541c57e23225681986132b13078209f33cda
Ifaz Uddin <iu22@line03.doc.ic.ac.uk>
2023-11-06 12:34:19 +0000

    Moved int write() into syscall.c as this is the correct file for it

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -27,6 +27,47 @@ static void sys_seek(int fd, unsigned position);
 static unsigned sys_tell(int fd);
 static void sys_close(int fd);
 
+/* Writes size bytes from buffer to the open file fd. Returns the number of bytes actually
+written, which may be less than size if some bytes could not be written.
+Writing past end-of-file would normally extend the file, but file growth is not implemented
+by the basic file system. The expected behavior is to write as many bytes as possible up to
+end-of-file and return the actual number written, or 0 if no bytes could be written at all.
+Fd 1 writes to the console. Your code to write to the console should write all of buffer in
+one call to putbuf(), at least as long as size is not bigger than a few hundred bytes. (It is
+reasonable to break up larger buffers.) Otherwise, lines of text output by different processes
+may end up interleaved on the console, confusing both human readers and our grading scripts.*/
+
+/* Questions :
+- What can cause less bytes to be written?
+- How do I check for this?
+- Is it fine I return 0 if charBuffer is null
+
+Writes the N characters in BUFFER to the console.
+void
+putbuf (const char *buffer, size_t n)
+{
+  acquire_console ();
+  while (n-- > 0)
+    putchar_have_lock (*buffer++);
+  release_console ();
+}
+*/
+
+int write(int fd, const void *buffer, unsigned size) {
+  if (fd == 1) {
+    const char *charBuffer = (const char *)buffer;
+
+    // Checking buffer is not NULL, if NULL return 0 since no byte was written
+    if (charBuffer == NULL) {
+      return 0;
+    }
+
+    // Write to console using putbuf
+    putbuf(charBuffer, size);
+    return size;
+  }
+}
+
 void
 syscall_init (void) 
 {
