a7a648672ca7a84cda8568dcf427dcab157cb078
Shaban Kashif <mk2520@imperial.ac.uk>
2023-11-06 13:22:09 +0000

    Sys_halt() implemented in syscall.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -170,6 +170,10 @@ void syscall_args_check(uint32_t *syscall_num, int args) {
   }
 }
 
+static void sys_halt(void) {
+  shutdown_power_off();
+}
+
 static bool access_user_mem (const void *uaddr) {
   if (is_user_vaddr(uaddr) && pagedir_get_page(thread_current()->pagedir, uaddr) != NULL) {
     return true;
@@ -181,6 +185,15 @@ static void sys_exit(int status) {
   printf ("%s: exit(%d)\n", thread_current()->name,status);
   thread_exit();
 }
+
+static void sys_exit(int status);
+static pid_t sys_exec(const char *file);
+static int sys_wait(pid_t pid);
+static bool sys_create(const char *file, unsigned initial_size);
+static bool sys_remove(const char *file);
+static int sys_open(const char *file);
+static int sys_filesize(int fd);
+static int sys_read(int fd, void *buffer, unsigned size);
 
 static int sys_write(int fd, const void *buffer, unsigned size) {
   if (fd == 1) {
@@ -196,3 +209,7 @@ static int sys_write(int fd, const void *buffer, unsigned size) {
     return size;
   }
 }
+
+static void sys_seek(int fd, unsigned position);
+static unsigned sys_tell(int fd);
+static void sys_close(int fd);
